8223e194cdaa18417e0f4a5a6ac64e6f492299ed
Arjun Bhujel <arjunbhujel600@gmail.com>
2023-12-30 15:11:40 +0545

    Minor Changes in Code

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
+
 var morgan = require("morgan")
 
-app.use(express.static("build"))
-
+app.use(cors())
 app.use(express.json())
+app.use(express.static("build"))
 
 morgan.token("body", function (req, res) {
   // console.log(req.body)
